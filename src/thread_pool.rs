5c6bfec1a25156d2cb0d98192d6ab706de4e699c
lcs <lcs.shun@foxmail.com>
2024-04-08 23:32:39 +0800

    add thread pool code

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -2,6 +2,13 @@ pub struct ThreadPool;
 
 impl ThreadPool {
     pub fn new(size: usize) -> Self {
+        Self{}
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
         
     }
 }
